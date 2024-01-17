a1249ce9f03d77547cc766345f92fc18f1f8d81f
Raman Shakya <raman.gwarko15@gmail.com>
2024-01-17 23:01:12 +0545

    simple grid to work with

---
+++ src/Grid-Component/gridClass.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./grid.css";
+
+class Grid extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.x = 10;
+        this.y = 10;
+        
+        let temp = [];
+        for (let i = 0; i < this.x; i++) {
+            let tempLine = [];
+            for (let j = 0; j < this.y; j++) tempLine.push(0);
+            temp.push(tempLine);
+        }
+
+        this.state = {
+            grid: temp,
+        };
+
+    }
+    
+    // initializeGrid() {
+    //     console.log("initializing grid");
+    //     const temp = [
+    //         [1,2,3],
+    //         [3,5,5]
+    //     ]
+    //     this.setState(
+    //         {grid: temp}
+    //     )
+    // }
+
+    setPixel(x, y) {
+        if (this.state.grid[x][y]) this.state.grid[x][y] = 0;
+        else this.state.grid[x][y] = 1;
+
+        this.setState({
+            grid: this.state.grid
+        });
+    }
+
+    unSetPixel(x, y) {
+        this.grid[x][y] = 0;
+    }
+
+
+    render() {
+        return <div className="pixel-grid">
+            {   this.state.grid && 
+                this.state.grid.map((line, x)=>
+                    <div className="pixel-line" key={x}>
+                        {line.map((pixData, y)=>
+                            <div className={`pixel ${pixData>0?'colored-pixel':''}`} onClick={()=>this.setPixel(x,y)} key={y}></div>
+                        )}
+                    </div>
+                )
+            }
+        </div>
+    }
+    
+
+}
+
+
+export default Grid;
